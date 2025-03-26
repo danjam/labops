0361fe3c092c7e8188ad7f58f399ec9d1a9b99ed
danjam <danjam@users.noreply.github.com>
2025-03-26 20:51:49 +0000

    Cleanup

--- run.sh
+++ run.sh
@@ -1,2 +1,16 @@
-export ANSIBLE_CONFIG=./ansible.cfg
-ansible-playbook playbooks/ubuntu_update.yml playbooks/docker_compose_deploy.yml -kK
+#!/bin/bash
+set -e
+
+export ANSIBLE_CONFIG="./ansible.cfg"
+
+VAULT_FILE="./.vault_password"
+VAULT_OPTION="--ask-vault-pass"
+
+if [ -f "$VAULT_FILE" ] && [ -r "$VAULT_FILE" ]; then
+    VAULT_OPTION="--vault-password-file $VAULT_FILE"
+fi
+
+# ansible-playbook -kK playbooks/ubuntu_update.yml playbooks/docker_compose_deploy.yml
+
+ ansible-playbook $VAULT_OPTION -kK playbooks/labops.yml -kK
+
